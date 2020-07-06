71661f3a82299179f6924826caff66e1f1c458d2
Martin Kemp <martinkemp@deloitte.co.uk>
2020-07-06 13:31:39 +0100

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,14 @@
 module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  plugins: ["@typescript-eslint"],
   extends: [
+    "eslint:recommended",
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parserOptions: {
-    ecmaVersion: 2019, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
-  },
-  rules: {
-    "@typescript-eslint/camelcase": "off", // Octokit uses snake case often
-    "@typescript-eslint/ban-ts-ignore": "off", // Octokit has incomplete events and ts-ignore is used to work around this
   },
 };
